eb9799ed0bc73632b2ebb47354f064bfcc291109
Richard Li <richardli125@gmail.com>
2019-07-23 17:57:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,121 +1,106 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const pageDiv = document.querySelector('#toy-collection')
 let addToy = false
 
-// YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
-    // submit listener here
   } else {
     toyForm.style.display = 'none'
   }
 })
 
-const list = document.querySelector('#toy-collection');
 
-function displayToys(event) {
-  event.forEach(function(toy){
-    const div = document.createElement('div');
-    div.className = 'card';
-    div.id=`id-${toy.id}`
-    const h2 = document.createElement('h2');
-    h2.innerText = toy.name;
-    div.innerHTML = `<img src="${toy.image}" class="toy-avatar"> <button type='button' class='like-btn'>Like</button>`;
-    const p = document.createElement('p');
-    p.innerText = toy.likes;
-    div.append(p);
-    div.append(h2);
-    list.append(div);
-    let button= p.parentNode.querySelector("button")
+fetch('http://localhost:3000/toys/')
+.then(resp => resp.json())
+.then(showToysOnDiv)
 
-    likeevent(button)
+addNewToy()
+addLikesWithButton()
 
-  })
+function showToysOnDiv(data) {
+  for(toy of data) {
+    const cardDiv = document.createElement('div')
+    cardDiv.className = 'card'
+    cardDiv.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button id=${toy.id} class="like-btn">Like <3</button>
+    `
+    pageDiv.append(cardDiv)
+    // console.log(toy)
+  }
 }
 
+function addNewToy() {
+  const newToyButtonName = document.querySelector('input[name= "name"]');
+  const newToyButtonImage = document.querySelector('input[name= "image"]');
 
-// OR HERE!
-
-fetch("http://localhost:3000/toys")
-.then(response => response.json())
-.then (displayToys)
-
-            function rickandmorty(name, image){
-            fetch("http://localhost:3000/toys",{
-              method:"POST",
-              headers:{
-                "Content-Type":"application/json",
-                "Accept": "application/json"
-              },
-              body:
-              JSON.stringify({
-                "name": `${name}`,
-                "image": `${image}`,
-                "likes": 0
-              })
-            }).then(promise => promise.json())
-            .then(data => console.log(data))
-
-            }
-
-const form = document.querySelector('form');
-
-function createNewToy() {
-  
-}
-
-form.addEventListener("submit",function(event){
-  console.log("richard")
- 
-  const div = document.createElement('div');
-  div.className = 'card';
-  const h2 = document.createElement('h2');
-  h2.innerText = form.name.value;
-  div.innerHTML = `<img src="${form.image.value}" class="toy-avatar"> <p>${0}</p> <button type='button' class='like-btn'>Like</button>`;
-  div.append(h2);
-  list.append(div);
-  event.preventDefault()
-  rickandmorty(form.name.value, form.image.value)
-  let button =h2.parentNode.querySelector("button")
-    likeevent(button)
-})
-
-
-function likeevent(button) {
-  button.addEventListener("click",function(){
-    increaselike(button.parentNode.id.split("-")[1])
-  })
-}
-
-
-
-function increaselike(id){
-  let likenow = parseInt(document.querySelector(`#id-${id}`).querySelector("p").innerText)
-  
-  fetch("http://localhost:3000/toys/"+id,{
-    method: "PATCH",
-    headers: {
-      "Content-Type":"application/json",
-      "Accept": "application/json"
-    },
-    body:
-    JSON.stringify({
-      "likes": `${likenow+1}`
+  toyForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    fetch(`http://localhost:3000/toys/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "name": `${newToyButtonName.value}`,
+        "image": `${newToyButtonImage.value}`,
+        "likes": "0"
+      })
     })
-  }).then(response => response.json())
-  .then(function (data){
-    updatingLike(id,likenow)})
-
-
-  // .then(response => response.json())
-  // .then(console.log(data))
-
+    .then(resp => resp.json())
+    .then(function(data){
+      const cardDiv = document.createElement('div')
+      cardDiv.className = 'card'
+      cardDiv.innerHTML = `
+      <h2>${data.name}</h2>
+      <img src="${data.image}" class="toy-avatar" />
+      <p>${data.likes}Likes</p>
+      <button id=${data.id} class="like-btn">Like <3</button>
+    `
+      pageDiv.append(cardDiv)
+    })
+  })
 }
 
-function updatingLike(id,likenow) {
- document.querySelector(`#id-${id}`).querySelector("p").innerText=likenow+1
+// addLikesWithButton()
+
+function addLikesWithButton() {
+  
+  pageDiv.addEventListener('click', (event) => {
+    if (event.target.classList.contains('like-btn')) {
+      // const cardDiv = pageDiv.querySelector('.card')
+      let cardP = event.target.parentElement.querySelector('p')
+      let number = parseInt(cardP.innerText.split(" ")[0])
+            // debugger
+      fetch(`http://localhost:3000/toys/${event.target.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          'likes': `${number + 1}`
+        })
+      })
+      .then(resp => resp.json())
+      .then(function() {
+        cardP.innerText = `${++number} Likes`
+      })
+    }
+  })
 }
+
+//1. fix your consts - try to only grab and assign once 
+//2. try putting them at the top all together so you can reference them all together, afterwards you can refactor to grab them only in lower scopes if you need
+//3. Likes: <span>${ num }</span>
+// ^^ do that (it makes it way easier to update a number without [0])
+// add a dataset id to the span (toy-like-btn-${ toy.id })
